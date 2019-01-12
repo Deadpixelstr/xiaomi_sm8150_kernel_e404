9b381960fbae46053fe5f747781d6108abacbc3f
Bryan Huntsman <bryanh@codeaurora.org>
2019-01-11 16:49:48 -0800

    Use 'GPL-2.0-only' as the SPDX-License-Identifier tag
    s/SPDX-License-Identifier: GPL-2.0/SPDX-License-Identifier: GPL-2.0-only/
    
    Change-Id: I0558c468c25d8229af2c8caade25c458875913f4
    Signed-off-by: Bryan Huntsman <bryanh@codeaurora.org>

--- drivers/soc/qcom/smem.c
+++ drivers/soc/qcom/smem.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2015, Sony Mobile Communications AB.
  * Copyright (c) 2012-2013, 2017, 2019-2020 The Linux Foundation. All rights reserved.
